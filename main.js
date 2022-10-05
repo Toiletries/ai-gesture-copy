8a54631aface5e315d2d885aa132392a82607178
Toiletries <92912011+Toiletries@users.noreply.github.com>
2022-10-05 18:21:35 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function setup(){
+canvas=createCanvas(600,500)
+canvas.center()
+video=createCapture(VIDEO)
+}
+function draw(){
+background("grey")
+fill("black")
+textSize(100)
+text("peter",300,200)
+}
